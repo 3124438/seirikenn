e0add827d7d209d77b657aa7b379bc5fa433cb5e
刺し身 <3124438@gse.okayama-c.ed.jp>
2025-12-28 22:41:01 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // エラーチェックを無視
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // ↓↓↓ ここが今回の修正ポイントです ↓↓↓
+  // "undici" という部品を強制的に読み込ませる設定
   transpilePackages: ['undici', 'firebase', '@firebase/auth'],
+  
+  // さらに、スマホ用の画面を作るときは "undici" を完全に無視させる設定（ここが最強！）
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias['undici'] = false;
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
